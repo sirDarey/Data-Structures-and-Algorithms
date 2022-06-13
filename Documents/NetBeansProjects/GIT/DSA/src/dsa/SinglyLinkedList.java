d3d1e65a3653f5bd3a99a1cd1317c4e8d31b0ded
Sir Darey <darey1oguns@gmail.com>
2022-06-13 21:15:58 +0100

    First Commit for Singly Linked List

---
+++ Documents/NetBeansProjects/GIT/DSA/src/dsa/SinglyLinkedList.java
@@ -0,0 +1,44 @@
+package dsa;
+/**
+ *
+ * @author Sir Darey
+ */
+public class SinglyLinkedList {
+    private ListNode head;
+    
+    private static class ListNode  {
+        private int data;
+        private ListNode  next;
+        
+        public ListNode (int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    
+    public void display () {
+        ListNode current = head;
+        
+        while (current!=null) {
+            System.out.print(current.data + " --> ");
+            current = current.next;
+        }
+        
+        System.out.println("null");
+    }
+    
+    public static void main(String[] args) {
+       SinglyLinkedList sll = new SinglyLinkedList();
+       sll.head = new ListNode(10);
+       ListNode second = new ListNode(1);
+       ListNode third = new ListNode(8);
+       ListNode fourth = new ListNode(11);
+       
+       sll.head.next = second;
+       second.next = third;
+       third.next = fourth;
+       
+       sll.display();
+    }
+    
+}
